a781ccb5069f85c40f989e4c4aeb47c851a25bbe
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2024-01-13 18:48:55 -0700

    chore: fake test, surpress dead code warning"

--- back-end/src/utils/env.rs
+++ back-end/src/utils/env.rs
@@ -2,6 +2,7 @@ use color_eyre::eyre::Context;
 use color_eyre::Report;
 use url::Url;
 
+#[allow(dead_code)]
 pub fn get_env_as_url(key: &str) -> Result<Url, Report> {
     let value = std::env::var(key)?;
 
