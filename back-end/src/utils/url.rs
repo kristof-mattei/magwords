95b0d85e3072803dd8cb382d070983d12a5e5caf
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2024-01-13 19:12:14 -0700

    chore: segment test

--- back-end/src/utils/url.rs
+++ back-end/src/utils/url.rs
@@ -9,3 +9,30 @@ pub fn add_segments(mut base_url: Url, segments: &[&str]) -> Url {
 
     base_url
 }
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use url::Url;
+
+    use crate::utils::url::add_segments;
+
+    #[test]
+    fn test_add_single_segment() {
+        let url = Url::from_str("https://example.com").unwrap();
+
+        let new_url = add_segments(url, &["foobar"]);
+
+        assert_eq!(new_url.as_str(), "https://example.com/foobar");
+    }
+
+    #[test]
+    fn test_multple_segments() {
+        let url = Url::from_str("https://example.com").unwrap();
+
+        let new_url = add_segments(url, &["foo", "bar"]);
+
+        assert_eq!(new_url.as_str(), "https://example.com/foo/bar");
+    }
+}
