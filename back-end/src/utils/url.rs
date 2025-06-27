b64758ca35747ed0aa9d9b41498e2c7b977df97b
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2025-06-26 18:21:06 -0700

    fix: use expect instead of allow, it is more explicit

--- back-end/src/utils/url.rs
+++ back-end/src/utils/url.rs
@@ -1,6 +1,6 @@
 use url::Url;
 
-#[allow(dead_code)]
+#[expect(dead_code)]
 pub fn add_segments(mut base_url: Url, segments: &[&str]) -> Url {
     {
         let mut s = base_url.path_segments_mut().expect("URL is relative");
