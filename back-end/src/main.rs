cc4a90a342012676c9a6c836bab11e00c913377c
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2025-07-08 18:21:45 -0700

    chore: remove message, it's the default name

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -76,22 +76,22 @@ async fn start_tasks() -> Result<(), eyre::Report> {
     tokio::select! {
         r = utils::wait_for_sigterm() => {
             if let Err(e) = r {
-                event!(Level::ERROR, message = "Failed to register SIGERM handler, aborting", ?e);
+                event!(Level::ERROR, "Failed to register SIGERM handler, aborting", ?e);
             } else {
                 // we completed because ...
-                event!(Level::WARN, message = "Sigterm detected, stopping all tasks");
+                event!(Level::WARN, "Sigterm detected, stopping all tasks");
             }
         },
         r = signal::ctrl_c() => {
             if let Err(e) = r {
-                event!(Level::ERROR, message = "Failed to register CTRL+C handler, aborting", ?e);
+                event!(Level::ERROR, "Failed to register CTRL+C handler, aborting", ?e);
             } else {
                 // we completed because ...
-                event!(Level::WARN, message = "CTRL+C detected, stopping all tasks");
+                event!(Level::WARN, "CTRL+C detected, stopping all tasks");
             }
         },
         () = token.cancelled() => {
-            event!(Level::ERROR, message = "Underlying task stopped, stopping all others tasks");
+            event!(Level::ERROR, "Underlying task stopped, stopping all others tasks");
         },
     };
 
@@ -107,10 +107,7 @@ async fn start_tasks() -> Result<(), eyre::Report> {
         .await
         .is_err()
     {
-        event!(
-            Level::ERROR,
-            message = "Tasks didn't stop within allotted time!"
-        );
+        event!(Level::ERROR, "Tasks didn't stop within allotted time!");
     }
 
     event!(Level::INFO, "Goodbye");
