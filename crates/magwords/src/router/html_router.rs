9dd28669d912e3aeed21fcdbc9e0e51271e349b3
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-04-17 09:32:11 -0700

    chore: axum-proxy is deprecated

--- crates/magwords/src/router/html_router.rs
+++ crates/magwords/src/router/html_router.rs
@@ -6,13 +6,13 @@ pub fn build_html_router() -> Router {
     if let Ok(front_end_proxy) = std::env::var("FRONT_END_PROXY").as_deref() {
         event!(Level::INFO, "Serving website via proxy");
 
-        let vite_proxy_service_builder = axum_proxy::builder_http(front_end_proxy).unwrap();
+        let vite_proxy_service_builder = tower_proxy::builder_http(front_end_proxy).unwrap();
 
-        let svc: axum_proxy::ReusedService<
-            axum_proxy::Identity,
-            axum_proxy::client::HttpConnector,
+        let svc: tower_proxy::ReusedService<
+            tower_proxy::Identity,
+            tower_proxy::client::HttpConnector,
             axum::body::Body,
-        > = vite_proxy_service_builder.build(axum_proxy::rewrite::Identity {});
+        > = vite_proxy_service_builder.build(tower_proxy::rewrite::Identity {});
 
         Router::new().fallback_service(svc)
     } else {
