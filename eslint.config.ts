09ca501ad935534fb9beb3e8585450e50fcfe6ba
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-06-15 16:35:48 -0700

    chore: new lints

--- eslint.config.ts
+++ eslint.config.ts
@@ -229,16 +229,18 @@ const config: ReturnType<typeof defineConfig> = defineConfig(
             "perfectionist/sort-intersection-types": ["error"],
             "perfectionist/sort-union-types": ["error"],
 
-            // type order relies on perfectionist/sort-intersection-types
-            "unicorn/prefer-type-literal-last": ["off"],
-
             "unicorn/class-reference-in-static-methods": ["error", { preferThis: false, preferSuper: false }],
+
+            "unicorn/consistent-class-member-order": ["off"],
 
             // const go in top
             "unicorn/no-declarations-before-early-exit": ["off"],
 
             // this one doesn't consider types (e.g. `ip.is4() ? ip.to4().address : ip.address`, the `address` is not of the same type, yet it hits this rule)
             "unicorn/prefer-minimal-ternary": ["off"],
+
+            // type order relies on perfectionist/sort-intersection-types
+            "unicorn/prefer-type-literal-last": ["off"],
         },
     },
 
